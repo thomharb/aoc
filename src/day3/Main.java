461b1684969222843f7fc35754a0109f16bfb7b8
thomharb <thomharbers@gmail.com>
2021-12-03 10:19:41 +0100

    day3 p1 restructure + p2

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -22,7 +22,8 @@ public class Main {
                 input.add(line);
             }
 
-            solve();
+            solve1();
+            solve2();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,31 +33,81 @@ public class Main {
         System.out.printf("Execution time: %d ms", (duration / 1000000));
     }
 
-    public static void solve() {
+    public static void solve1() {
         int binaryWordLength = input.get(0).length();
 
         StringBuilder gamma = new StringBuilder();
         StringBuilder epsilon = new StringBuilder();
 
         for (int i = 0; i < binaryWordLength; i++) {
-            int counter = 0;
-            for (String word : input) {
-                if (word.charAt(i) == '1') {
-                    counter++;
-                }
-            }
-            if (counter > (input.size()) / 2) {
-                gamma.append("1");
-                epsilon.append("0");
-            } else {
-                gamma.append("0");
-                epsilon.append("1");
-            }
+            char[] common = mostCommonBit(input, i);
+            gamma.append(common[0]);
+            epsilon.append(common[1]);
         }
 
         int gammaDecimal = Integer.parseInt(gamma.toString(), 2);
         int epsilonDecimal = Integer.parseInt(epsilon.toString(), 2);
 
         System.out.println("gamma * epsilon = " + gammaDecimal * epsilonDecimal);
+    }
+
+    public static char[] mostCommonBit(ArrayList<String> input, int position) {
+        int counter = 0;
+        for (String word : input) {
+            if (word.charAt(position) == '1') {
+                counter++;
+            }
+        }
+        if (counter > (input.size() - 1) / 2) {
+            return new char[]{'1', '0'};
+        } else {
+            return new char[]{'0', '1'};
+        }
+    }
+
+    public static void solve2() {
+        int oxygen = Integer.parseInt(computeOxygen(), 2);
+        int co2 = Integer.parseInt(computeCO2(), 2);
+        System.out.println("oxygen * co2 = " + oxygen * co2);
+    }
+
+    public static String computeOxygen() {
+        ArrayList<String> subarray = input;
+        int binaryWordLength = subarray.get(0).length();
+
+        for (int i = 0; i < binaryWordLength; i++) {
+            char mostCommon = mostCommonBit(subarray, i)[0];
+            subarray = computeSubarray(subarray, i, mostCommon);
+
+            if (subarray.size() == 1) {
+                return subarray.get(0);
+            }
+        }
+        return "";
+    }
+
+    public static String computeCO2() {
+        ArrayList<String> subarray = input;
+        int binaryWordLength = subarray.get(0).length();
+
+        for (int i = 0; i < binaryWordLength; i++) {
+            char leastCommon = mostCommonBit(subarray, i)[1];
+            subarray = computeSubarray(subarray, i, leastCommon);
+
+            if (subarray.size() == 1) {
+                return subarray.get(0);
+            }
+        }
+        return "";
+    }
+
+    public static ArrayList<String> computeSubarray(ArrayList<String> subarray, int position, char common) {
+        ArrayList<String> wordsLeft = new ArrayList<>();
+        for (String word : subarray) {
+            if (word.charAt(position) == common) {
+                wordsLeft.add(word);
+            }
+        }
+        return wordsLeft;
     }
 }
