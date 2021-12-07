0fcbaed20bfe3a2e486fd51647cd7637a679fbcf
thomharb <thomharbers@gmail.com>
2021-12-07 13:15:27 +0100

    day7 p1

---
+++ src/day7/Main.java
@@ -0,0 +1,58 @@
+package day7;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    static ArrayList<Integer> input = new ArrayList<>();
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        try {
+            File file = new File("src/day7/input.txt");
+//             File file = new File("src/day7/example.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                for (String number : split) {
+                    input.add(Integer.parseInt(number));
+                }
+            }
+            solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        System.out.printf("Execution time: %d ms", (duration / 1000000));
+    }
+
+    public static void solve() {
+        int goal = (int) getMedian();
+        int fuel = 0;
+        for (Integer crab : input) {
+            fuel += Math.abs(crab - goal);
+        }
+
+        System.out.printf("Amount of fuel needed for position %d = %d\n", goal, fuel);
+    }
+
+    public static double getMedian() {
+        Collections.sort(input);
+        double median;
+        if (input.size() % 2 == 0)
+            median = ((double)input.get(input.size()/2) + (double)input.get(input.size()/2 - 1))/2;
+        else
+            median = (double) input.get(input.size()/2);
+        return median;
+    }
+}
